1aac67ff435b6ea91faa4af48e7ee9bce01d0f5c
Ace G <aceguevarra48@gmail.com>
2023-01-21 22:02:50 +0800

    inital commit

---
+++ server.js
@@ -0,0 +1,29 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const indexRoute = require("./routes/indexRoute");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+
+app.use("/", indexRoute);
+app.use(express.static(__dirname + "/public"));
+
+const conn = mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to Database"));
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
