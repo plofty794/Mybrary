81beb328026ec7ca85646b6f97b6553d38757f95
Ace G <aceguevarra48@gmail.com>
2023-01-25 10:19:21 +0800

    added some changes

--- routes/authorsRoute.js
+++ routes/authorsRoute.js
@@ -3,8 +3,20 @@ const router = express.Router();
 const Author = require("../model/author");
 
 //Authors route
-router.get("/", (req, res) => {
-    res.render("authors/index");
+router.get("/", async (req, res) => {
+    let searchOptions = {};
+    if (req.query.name != null || req.query.name !== "") {
+        searchOptions.name = new RegExp(req.query.name, "gi");
+    }
+    try {
+        const authors = await Author.find(searchOptions);
+        res.render("authors/index", {
+            authors: authors,
+            searchOptions: req.query,
+        });
+    } catch {
+        res.redirect("/");
+    }
 });
 
 // New Author route
@@ -13,17 +25,20 @@ router.get("/new", (req, res) => {
 });
 
 // Create Author route
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
     const author = new Author({
         name: req.body.name,
     });
-    author.save(
-        (err,
-        (newAuthor) => {
-            if (err) throw err;
-        })
-    );
-    res.send(req.body.name);
+    try {
+        const newAuthor = await author.save();
+        // res.redirect(`authors/${newAuthor.id}`);
+        res.redirect(`authors`);
+    } catch {
+        res.render("authors/new", {
+            author: author,
+            errorMessage: "Error creating author",
+        });
+    }
 });
 
 module.exports = router;
