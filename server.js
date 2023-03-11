af4dff2396a133a00f7a81cf7364239b2232e110
Ace G <aceguevarra48@gmail.com>
2023-03-11 08:27:57 +0800

    Added some changes

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to Database"));
